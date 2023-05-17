ec2a5c47c7a0976c33b81594edb99f30f1e09279
Zhixiang Yan <YAN767957598@GMAIL.COM>
2023-05-16 21:54:48 -0400

    init commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 
 //get root route
 app.get('/', (req,res) => {
-    res.send('hello world')
+    res.send('hello world!')
 })
 //starting the server on port 3000
 app.listen(3000, () => {
